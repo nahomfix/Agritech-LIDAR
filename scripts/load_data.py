1eb6e7fc56d01c96393bf6d97dcd9969ea960924
Nahom Bekele <nahom.fix@gmail.com>
2022-06-27 12:06:02 +0300

    feat: add 3D terrain plot

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import geopandas
 import laspy
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pdal
 import rasterio
@@ -131,4 +132,54 @@ class LoadData:
             ax=ax_hist,
         )
 
+        plt.show()
+
+    def plot_heatmap(self, year: str, data_dict: dict) -> None:
+        """
+        Plots a heatmap view of a terrain
+
+        Parameters
+        ---
+            year: str
+                the year of the desired geopandas data
+
+            data_dict: dict
+                the dictionary mapping of the year to its geopandas data
+
+        """
+
+        fig, ax = plt.subplots(1, 1, figsize=(20, 10))
+        fig.patch.set_alpha(0)
+        plt.grid("on", zorder=0)
+        data_dict[year].plot(
+            column="elevation_m", ax=ax, legend=True, cmap="terrain"
+        )
+        plt.xlabel("Longitude")
+        plt.ylabel("Latitude")
+        plt.show()
+
+    def plot_3D(self, year: str, data_dict: dict) -> None:
+        """
+        Plots a 3D view of a terrain
+
+        Parameters
+        ---
+            year: str
+                the year of the desired geopandas data
+
+            data_dict: dict
+                the dictionary mapping of the year to its geopandas data
+
+        """
+
+        fig, ax = plt.subplots(1, 1, figsize=(20, 10))
+        fig.set_size_inches(18.5, 10.5, forward=True)
+        ax = plt.axes(projection="3d")
+        x = data_dict[year].geometry.x
+        y = data_dict[year].geometry.y
+        z = data_dict[year].elevation_m
+        points = np.vstack((x, y, z)).transpose()
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2], s=0.01)
+        ax.set_xlabel("Longitude")
+        ax.set_ylabel("Latitude")
         plt.show()
